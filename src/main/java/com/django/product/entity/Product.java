7156b9f2c901f4953a1c6d7bcd39dc9a67153480
kamalrana03 <kamalrana03@gmail.com>
2024-01-10 08:00:33 +0530

    product service init

---
+++ src/main/java/com/django/product/entity/Product.java
@@ -0,0 +1,57 @@
+package com.django.product.entity;
+
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Document
+public class Product {
+    private String id;
+    private String name;
+    private String description;
+    private List<String> tags = new ArrayList<>();
+    @DBRef
+    private Category category;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+}
